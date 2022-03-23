a9f1907e6b19e6bb747c7f066a45c659acfcc64f
btekin <burak.tekin@lunatr.com>
2022-03-23 14:09:59 +0300

    +Add Homework 2-1 +Add Homework 2-2 +Add Homework 2-3

---
+++ Homework-2-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] sayilar = new double[20];
+
+            Console.WriteLine("Lütfen 20 adet sayı giriniz: ");
+            for (int i = 0; i < 20; i++)
+            {
+                double deger;
+                if (double.TryParse(Console.ReadLine(), out deger))
+                {
+                    sayilar[i] = deger;
+                }
+            }
+            Array.Sort(sayilar);
+
+
+            double buyuk_uc_sayi_toplami = 0, kucuk_uc_sayi_toplami = 0;
+
+            Console.WriteLine(Environment.NewLine + "En küçük 3 sayı: {0} {1} {2}", sayilar[0], sayilar[1], sayilar[2]);
+            kucuk_uc_sayi_toplami = sayilar[0] + sayilar[1] + sayilar[2];
+            Console.WriteLine("En küçük 3 sayı Toplamı: {0}   Ortalaması: {1}", kucuk_uc_sayi_toplami, (kucuk_uc_sayi_toplami / 3).ToString("F2"));
+
+            Console.WriteLine(Environment.NewLine + "En büyük 3 sayı: {0} {1} {2}", sayilar[19], sayilar[18], sayilar[17]);
+            buyuk_uc_sayi_toplami = sayilar[19] + sayilar[18] + sayilar[17];
+            Console.WriteLine("En büyük 3 sayı Toplamı: {0}   Ortalaması: {1}", buyuk_uc_sayi_toplami, (buyuk_uc_sayi_toplami / 3).ToString("F2"));
+        }
+    }
+}
---
+++ Homework-2-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen sesli harflerinin taranmasını istediğiniz cümleyi yazınız");
+            string data = Console.ReadLine();
+            string sesli = "aeıioöuü";
+            char[] characters = new char[100];
+            int index = 0;
+            string sesli_harfler = "";
+            foreach (char a in data)
+            {
+                if (sesli.Contains(a))
+                {
+                    characters[index] = a;
+                    sesli_harfler += a + " ";
+                    index++;
+                }
+            }
+
+            Console.WriteLine(Environment.NewLine + "Girilen cümledeki sesli harfler: " + sesli_harfler);
+        }
+    }
+}
